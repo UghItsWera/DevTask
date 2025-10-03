2cae812b6c9457344c87bc44b28c8f72f0da53b9
UghItsWera <weronika.michalek1@gmail.com>
2025-10-03 18:44:26 +0100

    Front End

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,15 @@
-﻿// basic js initialization
-document.addEventListener('DOMContentLoaded', function(){
-    // other JS
-  });  
+﻿$(function() {
+  $('.hero-slider').slick({
+      dots: true,
+      arrows: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 6000,
+      fade: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      adaptiveHeight: false,
+      prevArrow: '<button type="button" class="slick-prev" aria-label="Previous slide">‹</button>',
+      nextArrow: '<button type="button" class="slick-next" aria-label="Next slide">›</button>'
+  });
+});
