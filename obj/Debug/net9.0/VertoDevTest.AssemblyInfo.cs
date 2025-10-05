ff5edf02f8459e9c36c1759b42aca4395790ff9b
UghItsWera <weronika.michalek1@gmail.com>
2025-10-05 13:29:25 +0100

    Adding Social Media Icons

--- obj/Debug/net9.0/VertoDevTest.AssemblyInfo.cs
+++ obj/Debug/net9.0/VertoDevTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VertoDevTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa52c72bde815ff9b2635420b5afc12fcb243a2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9c712161ea69370186227542cb00a481115c0f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VertoDevTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VertoDevTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
