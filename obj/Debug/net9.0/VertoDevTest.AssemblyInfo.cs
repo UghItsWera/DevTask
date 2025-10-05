4941a10833c193d20a0e40ace6b4df464458bcf9
UghItsWera <weronika.michalek1@gmail.com>
2025-10-05 11:24:32 +0100

    Commit

--- obj/Debug/net9.0/VertoDevTest.AssemblyInfo.cs
+++ obj/Debug/net9.0/VertoDevTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VertoDevTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeab1ba09c899a9f6291a8e4bca3f77ee5888d7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f82ba0f89a4b775de570095b860e3b284389c1f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("VertoDevTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VertoDevTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
